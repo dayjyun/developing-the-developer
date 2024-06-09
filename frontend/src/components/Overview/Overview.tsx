70b46471a0552575610c36e932e19ff9f90191b8
Kevin Barrios <kevinbarrios@ymail.com>
2024-06-08 21:22:34 -0500

    Updates

--- frontend/src/components/Overview/Overview.tsx
+++ frontend/src/components/Overview/Overview.tsx
@@ -1,23 +1,24 @@
 import "./Overview.scss"
+import sdeLogo from "../../assets/images/sde-logo.png"
 
 export default function Overview() {
     return (
       <div className="overview">
         {/* <h2>Overview</h2> */}
-        <h2>
+        <img src={sdeLogo} alt="SDE logo" />
+        <h3>
           <em>
             SDE <s>Portal</s> Platform
           </em>
-        </h2>
+        </h3>
         <p>SDE's flagship product</p>
         <p>Released November 17, 2023</p>
-        <p>Accelerators</p>
-        <p>User friendly interface; backed by user testing</p>
+        <p>Java and Angular Accelerators</p>
 
-        <p>What normally would take 14 weeks, now takes 2 weeks</p>
-        <p>Cut down in the amount of questions asked from 120+ to ~20.</p>
         <br />
-
+        <p>User friendly interface; backed by user testing</p>
+        <p>Cut down in the amount of questions asked from 120+ to ~20.</p>
+        <p>What normally would take 14 weeks, now takes 2 weeks</p>
       </div>
     );
 }
